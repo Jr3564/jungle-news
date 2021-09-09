20cdd470d461a81a2124b32f93490000a9e56bc1
jr3564 <rodolfojr94@gmail.com>
2021-09-09 14:28:11 -0300

    Specialized CRUDModel class for user models

---
+++ src/model/models/UsersModel.js
@@ -0,0 +1,85 @@
+const Users = require("../database/objectRelationalMapper/Users");
+const CRUDModel = require("./CRUDModel");
+
+module.exports = class extends CRUDModel {
+  constructor() {
+    super(Users);
+    this.keysMap = {
+      name: "use_name",
+      id: "use_id",
+      login: "use_login",
+      password: "use_password",
+      accessLevelName: "ace_name",
+      accessLevelId: "access_level_id",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    };
+    this.defaultReturnKeys = Object.keys(this.keysMap);
+  }
+
+  async create(newUser) {
+    const returnKeys = this.keysRename(
+      ["name", "id", "login", "accessLevelId"],
+      this.keysMap
+    );
+
+    return await this._Instance
+      .query()
+      .insert(this.mapAndTranslateKeys(newUser, this.keysMap))
+      .returning(...returnKeys);
+  }
+
+  getAll() {
+    const returnKeys = this.keysRename(
+      ["name", "id", "login", "accessLevelName"],
+      this.keysMap
+    );
+    return this._Instance
+      .query()
+      .innerJoin(
+        "access_levels as accessLevel",
+        "users.access_level_id",
+        "accessLevel.ace_id"
+      )
+      .select(...returnKeys);
+  }
+
+  getByLogin(userLogin) {
+    const returnKeys = this.keysRename(
+      ["name", "id", "login", "accessLevelName", "password", "accessLevelId"],
+      this.keysMap
+    );
+
+    return this._Instance
+      .query()
+      .where("use_login", userLogin)
+      .innerJoin(
+        "access_levels as accessLevel",
+        "users.access_level_id",
+        "accessLevel.ace_id"
+      )
+      .select(returnKeys);
+  }
+
+  updateById(userId, keyValue) {
+    const returnKeys = this.keysRename(
+      ["id", "login", ...Object.keys(keyValue)],
+      this.keysMap
+    );
+
+    return this._Instance
+      .query()
+      .where(this.keysMap.id, userId)
+      .patch(this.mapAndTranslateKeys(keyValue, this.keysMap))
+      .returning(returnKeys)
+      .first();
+  }
+
+  deleteById(userId) {
+    const returnKeys = this.keysRename(
+      ["id", "login", "name", "accessLevelId"],
+      this.keysMap
+    );
+    return this._deleteByKeyValue(this.keysMap.id, userId, returnKeys);
+  }
+};
