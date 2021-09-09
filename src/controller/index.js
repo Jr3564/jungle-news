67fe6a274a6236686e34529a426447b971440de0
jr3564 <rodolfojr94@gmail.com>
2021-09-09 14:59:16 -0300

    Created controller routing

---
+++ src/controller/index.js
@@ -0,0 +1,13 @@
+const Articles = require("./ArticlesController");
+const Categories = require("./CategoriesController");
+const Authors = require("./AuthorsController");
+const Users = require("./UserController");
+const AdminController = require("./AdminController");
+
+module.exports = {
+  Articles,
+  Categories,
+  Authors,
+  Users,
+  AdminController,
+};
