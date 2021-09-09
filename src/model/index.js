cd123dc626ffed8a154cee48a74036063b5344dc
jr3564 <rodolfojr94@gmail.com>
2021-09-09 15:13:41 -0300

    Created model routing

---
+++ src/model/index.js
@@ -0,0 +1,13 @@
+const AccessLevels = require("./models/AccessLevelsModel");
+const Users = require("./models/UsersModel");
+const Categories = require("./models/CategoriesModel");
+const Authors = require("./models/AuthorsModel");
+const Articles = require("./models/ArticlesModel");
+
+module.exports = {
+  AccessLevels,
+  Users,
+  Categories,
+  Authors,
+  Articles,
+};
