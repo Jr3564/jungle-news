e50d935b9b413bf43672379201c74bb5946ace5d
jr3564 <rodolfojr94@gmail.com>
2021-09-09 14:43:38 -0300

    Created model routing

---
+++ src/service/index.js
@@ -0,0 +1,13 @@
+const Articles = require("./ArticlesService");
+const Categories = require("./CategoriesService");
+const Authors = require("./AuthorsService");
+const Users = require("./UsersService");
+const Authentication = require("./AuthenticationService");
+
+module.exports = {
+  Articles,
+  Categories,
+  Authors,
+  Users,
+  Authentication,
+};
