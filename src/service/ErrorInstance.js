272b02f95edcc0929b9b7ae9208b7ebdfe12136b
jr3564 <rodolfojr94@gmail.com>
2021-09-10 14:56:16 -0300

    Created error specialization class

---
+++ src/service/ErrorInstance.js
@@ -0,0 +1,44 @@
+const statusCode = require("./statusCodes");
+
+class BadRequest {
+  constructor(message) {
+    this.message = message || "Bad Request";
+    this.statusCode = statusCode.BAD_REQUEST;
+  }
+}
+
+class UnprocessableEntity {
+  constructor(message) {
+    this.message = message || "Bad Request";
+    this.statusCode = statusCode.UNPROCESSABLE_ENTITY;
+  }
+}
+
+class NotFound {
+  constructor(message) {
+    this.message = message || "Not Found";
+    this.statusCode = statusCode.NOT_FOUND;
+  }
+}
+
+class Unauthorized {
+  constructor(message) {
+    this.message = message || "Unauthorized";
+    this.statusCode = statusCode.UNAUTHORIZED;
+  }
+}
+
+class InternalServerError {
+  constructor(message) {
+    this.message = message || "Internal Server Error";
+    this.statusCode = statusCode.INTERNAL_SERVER_ERROR;
+  }
+}
+
+module.exports = {
+  BadRequest,
+  NotFound,
+  Unauthorized,
+  UnprocessableEntity,
+  InternalServerError,
+};
